9570ba231c6d92b5dd54614e27efeb7daf23d195
Fahed Oudeh <fahed.oudeh@gmail.com>
2025-03-31 15:53:57 +0200

    opdracht klaar

--- Tuples.py
+++ Tuples.py
@@ -1,6 +1,8 @@
 
 steden = ("Amsterdam", "Rotterdam", "Utrecht", "Den Haag", "Eindhoven", "Groningen")
 # Loop door de tuple en print elk element afzonderlijk
+for stad in steden:
+    print(stad)
 
 # --------------------------------------------------------------------------------------------
 
@@ -8,24 +10,31 @@ tuple1 = (1, 2, 3)
 tuple2 = (4, 5, 6)
 
 # Combineer de twee tuples tot één nieuwe tuple
-
-
+nieuwe_tuple = tuple1 + tuple2
+print(nieuwe_tuple)
 # --------------------------------------------------------------------------------------------
 
 
 # Definieer een tuple met verschillende soorten gegevens (bijvoorbeeld getallen, strings, booleans, etc.)
+mijn_tuple = ("Amsterdam", 33, False, 1.9,  "Rotterdam", "Zeeland", True)
 
-
-# Print enkele elementen van de tuple namelijk het eerste, een subset (vanaf index 2 tot index 5) en het laatste element
-
+# Print enkele elementen van de mijn_tuple namelijk het eerste, een subset (vanaf index 2 tot index 5) en het laatste element
+print(mijn_tuple[0])
+new_mijn_tuple = mijn_tuple[2:5]
+print(new_mijn_tuple)
+print(mijn_tuple[0])
+print(mijn_tuple[-1])
 # --------------------------------------------------------------------------------------------
 
 
 # Maak een tuple met een naam en een leeftijd
-
+gegevens = ("Ali",8)
 
 # Pak de gegevens uit de tuple en sla ze op in afzonderlijke variabelen (Wat gebeurt er als je de variabelen in de verkeerde volgorde definieert?)
+naam, leeftijd = gegevens
 
 
 # Print de uitgepakte variabelen
 
+print("naam:", naam)
+print("leeftijd:", leeftijd)
--- loops.py
+++ loops.py
@@ -3,6 +3,7 @@
 # Schrijf een programma dat de tafel van een getal afdrukt.
 # De gebruiker voert een getal in en het programma drukt de tafel van dat getal af.
 # De tafel van 7 ziet er bijvoorbeeld als volgt uit:
+
 # 7 x 1 = 7
 # 7 x 2 = 14
 # 7 x 3 = 21
@@ -16,10 +17,25 @@
 
 
 # Probeer het eerst zonder loop,
+getal = int(input("voer een getal in:\n"))
+
+
+# print(getal, "x 1 =", getal * 1)
+# print(getal, "x 2 =", getal * 2)
+# print(getal, "x 3 =", getal * 3)
+# print(getal, "x 4 =", getal * 4)
+# print(getal, "x 5 =", getal * 5)
+# print(getal, "x 6 =", getal * 6)
+# print(getal, "x 7 =", getal * 7)
+# print(getal, "x 8 =", getal * 8)
+# print(getal, "x 9 =", getal * 9)
+# print(getal, "x 10 =", getal * 10)
+
 
 
 # Probeer het nu met een loop.
-
+for getal1 in range(1, 11):
+    print(f"{getal} x {getal1} = {getal*getal1}")
 
 # --------------------------------------------------------------------------------------------
 
@@ -28,6 +44,14 @@
 
 # Bijvoorbeeld: de som van alle getallen tot 3 is 6 (1 + 2 + 3 = 6)
 
+limiet = int(input("Voer een limiet in: "))
+
+som = 0
+
+for getal in range(1, limiet + 1):
+    som += getal
+
+print(f"De som van alle getallen tot {limiet} is {som}")
 
 
 # --------------------------------------------------------------------------------------------
@@ -40,7 +64,15 @@
 # En voor veelvouden van vijf, druk "Buzz" af.
 # Voor veelvouden van zowel drie als vijf, druk "FizzBuzz" af.
 
-
+for getal in range(1, 101):
+    if getal % 3 == 0 and getal % 5 == 0:
+        print("FIZZBUZZ")
+    elif getal % 3 == 0:
+        print("FIZZ")
+    elif getal % 5 == 0:
+        print("BUZZ")
+    else:
+        print(getal)
 
 # --------------------------------------------------------------------------------------------
 
@@ -60,13 +92,41 @@
 # 8 + 13 = 21
 # 13 + 21 = 34
 
-i = int(input("Hoeveel Fibonacci-getallen wil je zien? "))
+# i = int(input("Hoeveel Fibonacci-getallen wil je zien? "))
 
 # De eerste twee getallen van de Fibonacci-reeks zijn 0 en 1
-a = 0
-b = 1
+# a = 0
+# b = 1
 
 # Eerst drukken we de eerste twee getallen af
 
 
 # Vervolgens berekenen we de volgende getallen en drukken ze af
+# i = int(input("Hoeveel Fibonacci-getallen wil je zien? "))
+#
+# a = 0
+# b = 1
+#
+# print(a)
+# print(b)
+#
+# for _ in range(i - 2):
+#     c = a + b
+#     print(c)
+#     a = b
+#     b = c
+
+aantal_getallen = 10
+
+eerste = 0
+tweede = 1
+
+print("Fibonacci-reeks:")
+print(eerste)
+print(tweede)
+
+for _ in range(aantal_getallen - 2):
+    volgende = eerste + tweede
+    print(volgende)
+    eerste = tweede
+    tweede = volgende
